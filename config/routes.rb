ef46d46b6569eee7be176bb6406df4a5d5e49b3c
shreejana012 <shrijana460@gmail.com>
2016-10-06 17:23:06 +0545

    bug bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-   Rails.application.routes.draw do
 	root to: 'posts#index'
-end
     	resources :posts do
-		resources :comments
-		resources :upvotes, only: :create
-		resources :downvotes, only: :create
-	end
-    resources :users, only: :show 
+		 resources :comments
+		 resources :upvotes, only: :create
+		 resources :downvotes, only: :create
+	    end
+         resources :users, only: :show 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-end
 end
