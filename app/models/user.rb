36e2e521d080c0af1ed3d417f19fac13c4394d10
shreejana012 <shrijana460@gmail.com>
2016-10-25 16:47:43 +0545

    djfk

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   has_many :posts
   has_many :comments
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   acts_as_voter
   
 end
--- db/migrate/20161003042353_create_followings.rb
+++
@@ -1,8 +0,0 @@
-class CreateFollowings < ActiveRecord::Migration[5.0]
-  def change
-    create_table :followings do |t|
-
-      t.timestamps
-    end
-  end
-end
